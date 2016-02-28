c8dbf3d2ffeb0b1585ff2a375af8b2c536c3ca1d
Francis <francis.nana@standardbank.co.za>
2016-02-29 01:15:43 +0200

    initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('tangent')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('tangent', ['ngCookies', 'ngMessages', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
